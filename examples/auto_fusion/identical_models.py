03fa785f472868ba4457e79343d96e7d3bc8942a
Xin Li <nix.li@mail.utoronto.ca>
2021-04-13 15:47:47 +0000

    add contiguous

--- examples/auto_fusion/identical_models.py
+++ examples/auto_fusion/identical_models.py
@@ -5,30 +5,35 @@ from hfta.auto_fusion import fuse
 
 import time
 
-def get_fake_dataloader():
+def get_fake_dataloader(batch_size):
 
     dataset = torchvision.datasets.FakeData(
             size=1000,
             transform=torchvision.transforms.ToTensor()
             )
-    loader = DataLoader(dataset, batch_size=8)
+    loader = DataLoader(dataset, batch_size=batch_size)
     return loader
 
 if __name__ == "__main__":
     models = [
         torchvision.models.resnet18(pretrained=True),
-        torchvision.models.resnet50(pretrained=False)
+        torchvision.models.resnet18(pretrained=False)
         ]
 
+    batch_size = 8
     fused = fuse.fuse(models)
     fused.cuda()
-    loader = get_fake_dataloader()
-
+    loader = get_fake_dataloader(batch_size)
+    B = len(models)
     print("Staring training")
     start = time.time()
-    for d, l in loader:
-        d = d.cuda()
-        l = l.cuda()
-        fused(d)
+    for data, label in loader:
+        data = data.cuda()
+        data = data.unsqueeze(1).expand(-1, B, -1, -1, -1).contiguous()
+        label = label.cuda()
+
+        print(data.shape)
+        pred = fused(data)
+        print(pred.shape)
 
     print("Done, took {}s".format(time.time() - start))
